6ccde1a3f44c97b167d959bbd2c463caca3b16c1
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-11 10:58:04 +0530

    Implementing basic view skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
-import Deck from "./model/deck"
+import "./Pile.css";
+import Deck from "./model/deck";
+
+const PileBase = function() {
+  return <div className="PileBase" />;
+};
+
+const getPileBases = function(component) {
+  const pileBases = [];
+  for (let count = 0; count < 7; count++) {
+    pileBases.push(component);
+  }
+  return pileBases;
+};
 
 const App = function() {
-  return <div>{Deck.create().getCards()}</div>;
+  return (
+    <main>
+      <div className="top-container">
+        <div className="top-left-container"></div>
+        <div className="top-right-container"></div>
+      </div>
+      <div className="bottom-container">{getPileBases(<PileBase />)}</div>
+    </main>
+  );
 };
 
 export default App;
