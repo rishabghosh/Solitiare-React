929b6b8646e6b12cf5e18e705fccb33f27b81386
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-16 09:58:54 +0530

    now cards are not open if they are not top card

--- src/main/components/Card.js
+++ src/main/components/Card.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "../styles/Card.css";
 
+const CARD_BACK_UNICODE = "\u{1F0A0}";
+
+
 const Card = function(props) {
-  console.log(props.color);
+  let className = "card-main";
+  let unicode = CARD_BACK_UNICODE;
+
+  if (props.overlap) {
+    className += " overlapping";
+  }
+  
+  if(props.isTopCard){
+    unicode = props.unicode;
+  }
+
+  const style = { color: props.color };
+
   return (
-    <div className="card-main">
-      <div className="card-body" style={{ color: props.color }}>
-        {props.unicode}
+    <div className={className}>
+      <div className="card-body" style={style}>
+        {unicode}
       </div>
     </div>
   );
--- src/main/components/PileBase.js
+++ src/main/components/PileBase.js
@@ -4,6 +4,7 @@ import "../styles/Pile.css";
 import deck from "../models/deck";
 import _ from "lodash";
 
+//should be a method of deck where shuffler is passed as a param
 const wastePile = _.shuffle(deck.getCards()).slice(0, 28);
 
 let index = 0;
@@ -11,7 +12,12 @@ let index = 0;
 const getCards = function(cardCount) {
   const cards = [];
   for (let count = 1; count <= cardCount; count++) {
-    cards.push(<Card {...wastePile[index]} />);
+    const overlap = !(count === 1);
+    const isTopCard = count === cardCount;
+
+    cards.push(
+      <Card {...wastePile[index]} overlap={overlap} isTopCard={isTopCard} />
+    );
     index++;
   }
   return cards;
